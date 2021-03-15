7290838dc0b67ac7ab3d8cb36dd330b704885ecb
malekaridhi <malek.aridhii@gmail.com>
2021-03-15 20:16:43 +0100

    image

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+const fs = require('fs');
 require("dotenv").config();
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}))
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -18,3 +23,6 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
     console.log("MONGO connected on port 5050")
   }
   );
+
+  const PORT = process.env.PORT || 5050;
+app.listen(PORT, () => console.log(`the server is running on port : ${PORT}`));
