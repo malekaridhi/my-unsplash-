eebf6494efb02189b1e003941080d84e31a8003a
malekaridhi <malek.aridhii@gmail.com>
2021-03-16 10:31:22 +0100

    try

--- server/routers/img.js
+++ server/routers/img.js
@@ -1,22 +1,24 @@
-const Imagers =require("../model/image")
+const Images =require("../model/image")
 const router = require("express").Router();
 const express = require("express");
 const cloud = require("../cloudinary");
 const multer =require("../multer")
 const fs = require('fs');
 const path = require('path');
+
 router.post('/Images/upload', multer, async (req,res)=>{
-    console.log(req.files[0])
-    const result =await cloud.uploads(req.files[0].path)
+    const result = await cloud.uploads(req.body.pic);
     const imageinfo ={
-      Name : req.files[0].originalname,
+      Name : result.name,
       url: result.url
     }
-    const pics= new image (imageinfo)
+    const pics= new Images (imageinfo)
     pics.save()
-    fs.unlinkSync(req.files[0].path)
     res.json({
       msg:'done',
       pics:pics 
     })
   })
+
+  
+  module.exports = router;
